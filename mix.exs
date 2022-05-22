bf2db475db7bc5143a3a30244b804290b3399865
debora be <arobedbee@gmail.com>
2022-05-22 17:24:18 -0300

    deps for credo

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Wabanex.MixProject do
     [
       app: :wabanex,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -44,7 +44,8 @@ defmodule Wabanex.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
