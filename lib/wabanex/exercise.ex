3f29d33f4077c0647e14508dd3c83798573f176e
debora-be <arobedbee@gmail.com>
2022-06-01 21:03:15 -0300

    'exercise' and 'trainings' changeset

--- lib/wabanex/exercise.ex
+++ lib/wabanex/exercise.ex
@@ -1,20 +1,21 @@
-defmodule Wabanex.Training do
+defmodule Wabanex.Exercise do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias Wabanex.{User, Exercise}
+  alias Wabanex.Training
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
 
   @fields [:end_date, :start_date, :user_id]
 
-  schema "trainings" do
-    field :end_date, :date
-    field :start_date, :date
+  schema "exercises" do
+    field :name, :string
+    field :youtube_video_url, :string
+    field :protocol_description, :string
+    field :repetitions, :string
 
-    belongs_to :user, User
-    has_many :exercises, Exercise
+    belongs_to :training, Training
 
     timestamps()
   end
