6f518c499319f7db1b095e87584e122a2f41f8f7
debora-be <arobedbee@gmail.com>
2022-06-02 16:13:52 -0300

    updating 'training', 'root' and 'user' in types

--- lib/wabanex_web/schema/types/root.ex
+++ lib/wabanex_web/schema/types/root.ex
@@ -1,10 +1,17 @@
 defmodule WabanexWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
 
-  alias WabanexWeb.Resolvers.User, as: UserResolver
-  alias Crudry.Middlewares.TranslateErrors
+  alias WabanexWeb.Resolvers
 
-  import_types WabanexWeb.Schema.Types.User
+  alias Resolvers.User, as: UserResolver
+  alias Resolvers.Training, as: TrainingResolver
+
+  alias Crudry.Middlewares.TranslateErrors
+  alias WabanexWeb.Schema.Types
+
+  import_types Types.User
+  import_types Types.Training
+  import_types Types.Custom.UUID4
 
   object :root_query do
     field :get_user, type: :user do
@@ -19,6 +26,13 @@ defmodule WabanexWeb.Schema.Types.Root do
       arg :input, non_null(:create_user_input)
 
       resolve fn params, context -> UserResolver.create(params, context) end
+      middleware TranslateErrors
+    end
+
+    field :create_training, type: :training do
+      arg :input, non_null(:create_training_input)
+
+      resolve fn params, context -> TrainingResolver.create(params, context) end
       middleware TranslateErrors
     end
   end
